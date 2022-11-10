7c28bf1b1166f531f136b78ef5b0f1c7514d9caf
ThomasBerranger <tberranger@hotmail.fr>
2022-11-10 15:34:56 +0100

    filter button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,17 @@
 <template>
-  <i v-if="display === 'grid'" @click="display = 'edition'" class="fa-solid fa-plus-minus"></i>
-  <i v-else @click="display = 'grid'" class="fa-solid fa-user"></i>
-  
-  <i v-if="sortBy === 'quantity' && reverse === false" @click="sortBy = 'quantity'; reverse = true" class="fa-solid fa-arrow-down-short-wide"></i>
-  <i v-else-if="sortBy === 'quantity' && reverse === true" @click="sortBy = 'name'; reverse = false" class="fa-solid fa-arrow-down-wide-short"></i>
-  <i v-else-if="sortBy === 'name' && reverse === false" @click="sortBy = 'name'; reverse = true" class="fa-solid fa-arrow-down-a-z"></i>
-  <i v-else-if="sortBy === 'name' && reverse === true" @click="sortBy = 'quantity'; reverse = false" class="fa-solid fa-arrow-down-z-a"></i>
-  <br><br>
+  <div class="filter-container">
+    <button>
+      <i v-if="display === 'grid'" @click="display = 'edition'" class="fa-solid fa-list"></i>
+      <i v-else @click="display = 'grid'" class="fa-solid fa-plus-minus"></i>
+    </button>
+    
+    <button>
+      <i v-if="sortBy === 'quantity' && reverse === false" @click="sortBy = 'quantity'; reverse = true" class="fa-solid fa-arrow-down-short-wide"></i>
+      <i v-else-if="sortBy === 'quantity' && reverse === true" @click="sortBy = 'name'; reverse = false" class="fa-solid fa-arrow-down-wide-short"></i>
+      <i v-else-if="sortBy === 'name' && reverse === false" @click="sortBy = 'name'; reverse = true" class="fa-solid fa-arrow-down-a-z"></i>
+      <i v-else-if="sortBy === 'name' && reverse === true" @click="sortBy = 'quantity'; reverse = false" class="fa-solid fa-arrow-down-z-a"></i>
+    </button>
+  </div>
   <div :class="display === 'grid' ? 'products-container-3' : 'products-container-2'">
     <Product v-for="product in orderedProducts" :key="product.id" :product="product" :display="display"/>
   </div>
@@ -63,6 +68,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.filter-container {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+
+  & > button {
+    background-color: #593712;
+    border: none;
+    border-radius: 10px 15px 10px 15px;
+    padding: 5px 10px 5px 10px;
+    
+    & > i {
+      color: white;
+    }
+  }
+}
 .products-container{
   &-3 {
     display: grid;
